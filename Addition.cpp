5641f04a5a52d2d7e7a8c7c7806f03ee3315c2cd
Milkbaologist <gaiusmariuschen@gmail.com>
2022-07-10 21:51:48 +0800

    useless comments are gone

--- Addition.cpp
+++ Addition.cpp
@@ -1,12 +1,9 @@
-﻿// Addition.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
 #include <cstring>
 using namespace std;
 
 struct BigNumber {
-	/*char str[1000] = {0};*/
 	int numbers[1000] = {0};
 	int length = 0;
 
@@ -87,7 +84,6 @@ struct BigNumber {
 
 };
 
-
 int main() {
 	BigNumber a;
 	BigNumber b;
@@ -98,19 +94,5 @@ int main() {
 	cin >> str;
 	b.transfer(str);
 	BigNumber::multiply(a, b, result);
-	//cout << strtof(a.str, NULL) * strtof(b.str, NULL) << endl;
 	result.print();
 }
-
-
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: i
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
